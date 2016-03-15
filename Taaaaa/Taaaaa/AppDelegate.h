33d273ebabc722aa3ed27ec5aed1235417dc63e4
solobo <aaa@163.com>
2016-03-15 20:41:11 +0800

    assa
    asadads

---
+++ Taaaaa/Taaaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Taaaaa
+//
+//  Created by mac on 16/3/15.
+//  Copyright © 2016年 SoloBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
